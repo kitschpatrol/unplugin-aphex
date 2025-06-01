7060ceb2e3117c39d37d760d0e1ec70c27da0191
Eric Mika <eric@ericmika.com>
2025-06-01 14:33:02 -0400

    Add kpi shared config.

---
+++ eslint.config.ts
@@ -0,0 +1,9 @@
+import { eslintConfig } from '@kitschpatrol/eslint-config'
+
+export default eslintConfig({
+	// Enable as needed:
+	// astro: true,
+	// react: true,
+	// svelte: true,
+	// type: 'lib',
+})

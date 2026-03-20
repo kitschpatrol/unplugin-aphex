b7bc46f3807ca90f5930554037606d0c3bd47b77
Eric Mika <eric@ericmika.com>
2026-03-20 01:39:15 -0400

    Fix test collision.

--- test/api.node.test.ts
+++ test/api.node.test.ts
@@ -1,11 +1,15 @@
+import fs from 'node:fs'
 import { expect, it } from 'vitest'
 import { AphexExport } from '../src/api'
 
 it('works via the api ', async () => {
+	const cacheDirectory = './node_modules/.cache/aphex-node-test'
 	const identifier = '~aphex/Tiny/tiny'
-	const aphex = new AphexExport({ verbose: true })
+	const aphex = new AphexExport({ cacheDirectory, verbose: true })
 	expect(aphex.isIdentifier(identifier)).toBe(true)
 	expect(aphex.isIdentifier('not-an-aphex-identifier')).toBe(false)
 	const exportedPath = await aphex.exportPhoto(identifier)
-	expect(exportedPath).toContain('node_modules/.cache/aphex/tiny')
+	expect(exportedPath).toContain('node_modules/.cache/aphex-node-test/tiny')
+	await aphex.close()
+	fs.rmSync(cacheDirectory, { force: true, recursive: true })
 })
